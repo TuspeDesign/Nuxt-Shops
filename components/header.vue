8c4f38dfea8b127b9b10d36cbf26ef9d10484ccc
JerePalmu <jerepalmu03@gmail.com>
2022-01-31 12:28:07 +0200

    moved searchbar below <h1> from header

--- components/header.vue
+++ components/header.vue
@@ -6,30 +6,7 @@
             to="/"
             rel="home"
         >
-            <p class="py-2 sm:text-xl md:text-2xl font-bold text-white tracking-wider">VERKKOKAUPPOJA</p>
+            <p class="py-2 text-2xl font-bold text-white tracking-wider">VERKKOKAUPPOJA</p>
         </nuxt-link>
-
-        <input class="sm:w-64 md:w-96 bg-gray-200 text-gray-700 border border-black rounded-2xl py-2 px-4 leading-tight focus:outline-none focus:bg-white" type="text" v-model="search" placeholder="Hae verkkokauppaa">
-	    <div v-for="store in filteredStores" :key="store.id"/>
     </header>
 </template>
-
-<script>
-    export default {
-        data() {
-			return {
-				featured: null,
-				stores: [],
-				search: '',
-			};
-		},
-
-        computed: {
-            filteredStores: function(){
-				return this.stores.filter((store) => {
-					return store.title.match(this.search);
-				});
-			},
-		},
-    }
-</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,19 +1,22 @@
 <template>
-  <div class="mb-60">
-    <div class="container mx-auto text-center">
-      <h1 class="mt-40 text-6xl font-extrabold">ETUSIVU</h1>
+	<div class="mb-60">
+		<div class="container mx-auto text-center">
+			<h1 class="mt-40 text-6xl font-extrabold">ETUSIVU</h1>
 
-		<div class="mt-5 text-xl leading-tight">
-			Tervetuloa sivulle. Täältä voit etsiä suomalaisia verkkokauppoja,<br>niiden paikkakuntia ja paljon muuta.
+			<div class="mt-5 text-xl leading-tight">
+				Tervetuloa sivulle. Täältä voit etsiä suomalaisia verkkokauppoja,<br>niiden paikkakuntia ja paljon muuta.
+			</div>
+
+			<input class="mt-10 w-96 bg-gray-200 text-gray-700 border border-black rounded-2xl py-3 px-4 leading-tight focus:outline-none focus:bg-white" type="text" v-model="search" placeholder="Hae verkkokauppaa">
+	    	<div v-for="store in filteredStores" :key="store.id"/>
+
+			<ProductList 
+				v-if="featured" 
+        		:items="featured" 
+        		title="Näyteikkuna" 
+      		/>
 		</div>
-
-      <ProductList 
-        v-if="featured" 
-        :items="featured" 
-        title="Näyteikkuna" 
-      />
-    </div>
-  </div>
+	</div>
 </template>
 
 <script>
@@ -29,11 +32,17 @@
 			return {
 				featured: null,
 				stores: [],
+				search: '',
 			};
 		},
 		computed: {
 			site: function () {
 				return this.$store.getters["site"];
+			},
+			filteredStores: function(){
+				return this.stores.filter((store) => {
+					return store.title.match(this.search);
+				});
 			},
 		},
 		methods: {
